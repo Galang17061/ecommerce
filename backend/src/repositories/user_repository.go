188fe8989735e1f39b38f02ffecf15f0176edff6
Galang17061 <galang17061@gmail.com>
2025-03-22 18:19:06 +0800

    feat: implement find user by email method

--- backend/src/repositories/user_repository.go
+++ backend/src/repositories/user_repository.go
@@ -16,4 +16,13 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 
 func (r *UserRepository) Create(user *models.User) error {
 	return r.db.Create(user).Error
+}
+
+func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
 } 
