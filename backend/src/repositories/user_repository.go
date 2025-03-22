116b2aaf71fc2a86878346676ac763446312da83
Galang17061 <galang17061@gmail.com>
2025-03-22 18:23:10 +0800

    feat: add more user repository methods for CRUD operations

--- backend/src/repositories/user_repository.go
+++ backend/src/repositories/user_repository.go
@@ -38,4 +38,30 @@ func (r *UserRepository) ExistsByEmail(email string) (bool, error) {
 
 func (r *UserRepository) UpdatePassword(userID uint, hashedPassword string) error {
 	return r.db.Model(&models.User{}).Where("id = ?", userID).Update("password", hashedPassword).Error
+}
+
+func (r *UserRepository) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.db.First(&user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) Update(user *models.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *UserRepository) Delete(id uint) error {
+	return r.db.Delete(&models.User{}, id).Error
+}
+
+func (r *UserRepository) List(limit, offset int) ([]models.User, error) {
+	var users []models.User
+	err := r.db.Limit(limit).Offset(offset).Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 } 
