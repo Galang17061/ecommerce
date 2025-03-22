bcad3ee540d9efe3ca37ef3a66b7dd149dc7b6c2
Galang17061 <galang17061@gmail.com>
2025-03-22 12:56:11 +0800

    feat : initial backend commit

---
+++ backend/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Response{
+			Message: "Backend is healthy!",
+		})
+	})
+
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+} 
